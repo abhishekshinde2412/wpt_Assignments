45b1faca062d62b6377ac66ed40f9714eeba108b
abhishekshinde2412 <101059986+abhishekshinde2412@users.noreply.github.com>
2022-06-26 23:39:14 +0530

    Add files via upload

---
+++ DBassignment/que 5/accno.js
@@ -0,0 +1,37 @@
+let dbparams = {
+    host: 'localhost',
+    user: 'root',
+    password: 'cdac',
+    database: 'nashik',
+    port: 3306
+};
+const mysql = require('mysql2');
+const con = mysql.createConnection(dbparams);
+
+const express = require('express');
+const app = express();
+
+app.use(express.static("folder"));
+app.get("/getBal", (req, res) => {
+    let input = req.query.accno;
+    console.log(input);
+
+    let output = {
+        status: false,
+        itemdetails: { accno: 0, bal: "" }
+    };
+
+    con.query('select * from account where accno =?', [input], (error, rows) => {
+        if (rows.length > 0) {
+            output.status = true;
+            output.itemdetails = rows[0];
+        }
+        res.send(output);
+        console.log(output);
+
+    });
+
+
+});
+
+app.listen(990, function () { });
